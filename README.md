bf219abb10b8cb63f799d96174e1ffc72f7080f8
Victor Bateren <baterenvictor@gmail.com>
2021-07-19 11:52:14 +0100

    Working on header
    Started here

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # logistico
  
+I don't own the theme
+
+LogisticoVersion: 1.1.5
+By themepaw.com
+
+
+Enable auto-updates
+Logistico WordPress Theme is made for logistic services, transportation companies, packers and movers, corporate businesses, online companies in mind. It can fit as a small company website as well as a medium size business site. Logistico is fantastically designed to suit any blog/news or as a e-commerce website with WooCommerce. It is equiped with Elementor page builder and Themepaw Companion to make it more flexible and robust. Cross browser supported and Full responsive design to fit into any display sized devices and mobiles. WPML ready and 100% SEO friendly code can boost your site with Logistico WordPress Theme.
--- inc/hooks/general-hooks.php
+++ inc/hooks/general-hooks.php
@@ -100,44 +100,83 @@ endif;
 if( ! function_exists( 'logistico_header_wrap' ) ) :
 	function logistico_header_wrap() {
         ?>
-        <div class="col-lg-3 col-md-12 col-sm-6 col-xs-6 d-flex align-items-center">
-            <div class="site-branding">
-                <?php logistico_logo_wrap(); ?>
-            </div><!-- .site-branding -->
-            <button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'logistico' ); ?></button>
-        </div>
-        <div class="col-lg-9 col-md-12 col-sm-6 col-xs-6 d-flex align-items-center justify-content-end">
-            <div class="lgtico-nav-wrap lgtico-nav-wrap-2 lgtico-nav-wrap-3 lgtico-nav-wrap-4">
-                <div id="site-header-menu" class="site-header-menu">
-                    <?php 
-                    $nav_attr = array(
-                        'id' => 'site-navigation',
-                        'class' => array(
-                            'main-navigation',
-                            'lgtico-menu',
-                            'lgtico-menu-4',
-                            'lgtico-responsive-menu',
-                            'main-navigation'
-                        ),
-                        'aria-label' => esc_attr__( 'Top Menu', 'logistico' )
-                    );
-                    $nav_attr = apply_filters('logistico_nav_attr', $nav_attr);
-                    ?>
-                    <nav <?php echo logistico_set_attributes($nav_attr); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
-                        <?php do_action( 'logistico_nav_before' ); ?>
+        <!-- Header Topbar -->
+        <div class="top-bar font2-title1 white-clr">
+            <div class="theme-container container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-5">
+                        <ul class="list-items fs-10">
+                            <li><i class="fa fa-phone"></i> Call us now: <span class="theme-clr"> 081092837353636 </span></li>
+                            <li class="active"><i class="fa fa-envelope"></i> Email: <span class="theme-clr"> info@logistico.com </span></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 col-sm-7 fs-12">
                         <?php
-                        wp_nav_menu( array(
-                            'theme_location' => 'menu-1',
-                            'menu_id'        => 'primary-menu',
-                            'menu_class'     => 'main-menu',
-                            'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                        ) );
+                            if($weAreOpen) {
+                                if (date('l') == 'Saturday') {
+                                    $close_time = '1pm';
+                                }else{
+                                    $close_time = '5pm';
+                                }
+                            echo '<p class="contact-num" style="color: #00ff00; font-weight: 600;">Office Open | <span style="color: #e03418;"> Closes by '.$close_time.'</span></p>';
+                            } else {
+                            echo '<p class="contact-num" style="color: #e03418; font-weight: 600;">Office Closed | <span style="color: #00ff00;">Available Online</span></p>';
+                            }
                         ?>
-                        <?php do_action( 'logistico_nav_after' ); ?>
-                    </nav><!-- #site-navigation -->
+                    </div>
                 </div>
             </div>
-        </div> <!-- .col-lg-9 -->
+            <a href="https://logistico.com/wp-admin" class="sign-in fs-12 theme-clr-bg"> sign in </a>
+        </div>
+        <!-- /.Header Topbar -->
+
+        <!-- Header Logo & Navigation -->
+        <nav class="menu-bar font2-title1">
+            <div class="theme-container container">
+                <div class="row">
+                    <div class="col-md-2 col-sm-2">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-logo" href="<?= $baseurl; ?>"> <img src="assets/img/logo/logo-black.png" alt="logo" /> </a>
+                    </div>
+                    <div class="col-md-10 col-sm-10 fs-12">
+                        <div id="navbar" class="collapse navbar-collapse no-pad">
+                            <ul class="navbar-nav theme-menu">
+                                <li <?php echo ( $page_name == 'Home' || $page_parent == 'Home' ) ? 'class="active"' : ''; ?>><a href="<?= $baseurl; ?>">Home </a></li>
+                                <li <?php echo ( $page_name == 'About' || $page_parent == 'About' ) ? 'class="active"' : ''; ?>> <a href="about">about</a> </li>
+                                <li <?php echo ( $page_name == 'Track' || $page_parent == 'Track' ) ? 'class="active"' : ''; ?>> <a href="track"> track </a> </li>
+                                <li class="dropdown" <?php echo ( $page_name == 'Freights' || $page_parent == 'Freights' ) ? 'class="active"' : ''; ?>> 
+                                    <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" href="javascript:void(0)"> freights </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="air-freights">air freights</a></li>
+                                        <li><a href="sea-freights">sea freights</a></li>
+                                        <li><a href="project-cargo">project cargo</a></li>
+                                    </ul>
+
+                                </li>
+                                <li class="dropdown" <?php echo ( $page_name == 'Services' || $page_parent == 'Services' ) ? 'class="active"' : ''; ?>> 
+                                    <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" href="javascript:void(0)"> services </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="warehousing">ware housing</a></li>
+                                        <li><a href="personal-baggage">personal baggage</a></li>
+                                        <li><a href="custom-clearance">custom clearance</a></li>
+                                    </ul>
+
+                                </li>
+                                <li <?php echo ( $page_name == 'Contact' || $page_parent == 'Contact' ) ? 'class="active"' : ''; ?>> <a href="contact"> contact </a> </li>
+
+                                <li <?php echo ( $page_name == 'Career' || $page_parent == 'Career' ) ? 'class="active"' : ''; ?>> <a href="career"> career </a> </li>
+                            </ul>                                                      
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- /.Header Logo & Navigation -->
         <?php
     }
 endif;
